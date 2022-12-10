3109b23843d4d227393a38ea7527d3f688f9eb66
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-12-10 01:56:40 -0500

    chore: add nextjs experimental configurations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    externalDir: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   env: {
     API_ROUTE_URL: process.env.API_ROUTE_URL,
     API_ROUTE_PUBLIC_KEY: process.env.API_ROUTE_PUBLIC_KEY,
   },
+  disableExperimentalFeaturesWarning: true,
 }
 
 module.exports = nextConfig
